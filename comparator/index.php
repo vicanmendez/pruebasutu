5c03155709bc3201005222f140c06b6da8fdddbd
Víctor <vicanmendez@gmail.com>
2023-08-15 13:12:45 -0300

    Creating very simple form as frontend

--- comparator/index.php
+++ comparator/index.php
@@ -1,29 +1,57 @@
 <?php
 
 function handleComparatorEndpoint() {
-    $pages = isset($_GET['pages']) ? $_GET['pages'] : '';
+    $pages = isset($_GET['pages']) ? $_GET['pages'] : null;
     //convert pages to array
-    $pages = explode(',', $pages);
-    var_dump($pages);
+    if ($pages !== null) {
+        $pages = explode(',', $pages);
+    }
     $search = isset($_GET['search']) ? $_GET['search'] : '';
     $order = isset($_GET['order']) ? $_GET['order'] : '';
     $auction = isset($_GET['auction']) ? $_GET['auction'] : '';
     $buy_it_now = isset($_GET['buy_it_now']) ? $_GET['buy_it_now'] : '';
     $condition = isset($_GET['condition']) ? $_GET['condition'] : '';
+    $filters = isset($_GET['filters']) ? $_GET['filters'] : '';
     // Perform scraping based on the requested page
-    foreach ($pages as $page) {
+    if ($pages != null) { //si se piden muchas paginas
+        foreach ($pages as $page) {
+            switch ($page) {
+                case 'ebay':
+                    require_once '../pages/ebay.php';
+                    $ebayProducts = ebayScrappe($search, $order, $auction, $buy_it_now, $condition);
+                    echo json_encode($ebayProducts);
+                    break;
+                case 'amazon':
+                    require_once '../pages/amazon.php';
+                    $amazonProducts = amazonScrappe($search, $order, $filters);
+                    echo json_encode($amazonProducts);
+                    break;
+                case 'mercadolibre':
+                    require_once '../pages/mercadolibre.php';
+                    $mercadolibreProducts = mercadolibreScrappe($search, $order, $filters);
+                    echo json_encode($mercadolibreProducts);
+                    break;
+            }
+        }
+    }
+     //si no se pide una lista de paginas, al menos escaneamos la pagina solicitada
+    else {
+        $page = isset($_GET['page']) ? $_GET['page'] : '';
         switch ($page) {
             case 'ebay':
                 require_once '../pages/ebay.php';
                 $ebayProducts = ebayScrappe($search, $order, $auction, $buy_it_now, $condition);
+                echo json_encode($ebayProducts);
                 break;
             case 'amazon':
                 require_once '../pages/amazon.php';
                 $amazonProducts = amazonScrappe($search, $order, $filters);
+                echo json_encode($amazonProducts);
                 break;
             case 'mercadolibre':
                 require_once '../pages/mercadolibre.php';
                 $mercadolibreProducts = mercadolibreScrappe($search, $order, $filters);
+                echo json_encode($mercadolibreProducts);
                 break;
         }
     }
---
+++ getOrder.php
@@ -0,0 +1,63 @@
+<?php
+    $page = isset($_REQUEST["page"]) ? ($_REQUEST["page"]) : null;
+    if($page !== null) {
+        switch ($page) {
+            case 'ebay':
+                $array_search = [
+                    [
+                    "key" => "price-asc",
+                    "value" => "precio ascendente"
+                    ],
+                    [
+                    "key" => "price-desc",
+                    "value" => "precio descendente"
+                    ],
+                    [
+                    "key" => "ending-soonest",
+                    "value" => "finaliza pronto"
+                    ],
+                    ["key" => "most-recent",
+                    "value" => "más reciente"
+                    ],
+                    [
+                    "key" => "nearest",
+                    "value" => "más cercano"
+                    ]
+                ];
+                echo json_encode($array_search);
+                break;
+            case 'amazon':
+                $array_search = [
+                    [
+                    "key" => "price-asc",
+                    "value" => "precio ascendente"
+                    ],
+                    [
+                    "key" => "price-desc",
+                    "value" => "precio descendente"
+                    ],
+                    [
+                    "key" => "most-recent",
+                    "value" => "más reciente"
+                    ],
+                    [
+                    "key" => "review-rank",
+                    "value" => "más valorado"
+                    ]
+                ];
+                echo json_encode($array_search);
+                break;
+           default:
+                $array_search = [
+                    [
+                    "key" => "price-asc",
+                    "value" => "precio ascendente"
+                    ]
+                ];
+                echo json_encode($array_search);
+                break;
+        }
+    }
+
+
+?>
